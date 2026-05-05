6354dfe78dedb93e5e317b1c83b946ebb5320f4f
Felipe Jiménez <55604046+felipejimenez-ai@users.noreply.github.com>
2026-05-05 11:15:48 -0500

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.py
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -1,32 +1,12 @@
-from typing import List
-
-
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        """
-        I think that we could iterate the array and put each new number into a set.
-        Then if a number is already in the set, we return True.
-
-        Empty array -> False
-        All elements same -> True
-
-        [PSEUDOCODE]
-
-        Create an empty set (let’s call it seen).
-
-        Iterate through each number in the array:
-
-            - If the number is already in seen, return True (found a duplicate).
-
-            - Otherwise, add the number to seen.
-
-        If you finish the loop without finding a duplicate, return False.
-        """
-        
-        seen = set()
-
-        for num in nums:
-            if num in seen: return True
-            else: seen.add(num)
-        
+        counter = 0
+        counter_dict = {}
+        # iterate
+        for i in nums:
+            # count
+            if counter_dict.get(i) == None:
+                counter_dict[i] = 1
+            else:
+                return True
         return False
