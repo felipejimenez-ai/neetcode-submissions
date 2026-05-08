ab0a7662f861f7e62e271380e66d898206deceb8
Felipe Jiménez <55604046+felipejimenez-ai@users.noreply.github.com>
2026-05-08 11:49:56 -0500

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,21 +1,15 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        """
-        * Initialize a dictionary 
-        * Iterate the input list of words
-        * Key -> Will be the organized character's word 
-        * Value -> Will be the list of words
-        * Check if the organized characters' word are already in the dict
-        * Return the dictionary's values --list of words
-        """
-        
-        anagrams = defaultdict(list)
-
-        for w in strs:
-            w_sorted = ''.join(sorted(w))
-            anagrams[w_sorted].append(w)
-
-        return list(anagrams.values())
+        words = {}
+        for str in strs:
+            str_sorted = ''.join(sorted(str))
+            print(str)
+            if str_sorted in words.keys():
+                print(str)
+                words[str_sorted].append(str)
+            else:
+                words[str_sorted] = [str]
+        result = []
+        for items in words.values():
+            result.append(items)
+        return result
