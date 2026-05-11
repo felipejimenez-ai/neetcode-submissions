0739b061c52e4450e11eac4af1a99cd7af48ad28
Felipe Jiménez <55604046+felipejimenez-ai@users.noreply.github.com>
2026-05-11 11:16:46 -0500

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -1,35 +1,14 @@
-from collections import defaultdict
-from itertools import count
-from typing import List
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-      """
-      Count the frequency.
-      - Iterate the array.
-      - Use a dictionary.
-
-      Organize by highest frequency.
-      - Take the dictonary to a list.
-      - The list indexes represent the frequency.
-        - E.g. index 0 is zero frequency.
-
-      Return top k highest frequency.
-      - Return the top k highest frequency.
-      """
-
-      frequency = defaultdict(int)
-      for n in nums:
-        frequency[n] += 1
-
-      frequency_sorted = [[] for _ in range(len(nums) + 1)]
-      for number, freq in frequency.items():
-        frequency_sorted[freq].append(number)
-
-      result = []
-      for i in range(len(frequency_sorted) - 1, 0, -1):
-        if frequency_sorted[i]:
-          for j in frequency_sorted[i]:
-            result.append(j)
-            if len(result) == k:
-              return result
+        freq = {} # frequency of n in nums
+        res = []
+        for n in nums:
+            if n not in freq:
+                freq[n] = 1
+            else: 
+                freq[n] += 1
+        for i in range(k):
+            max_value = max(freq, key=freq.get)
+            freq.pop(max_value)
+            res.append(max_value)
+        return res
