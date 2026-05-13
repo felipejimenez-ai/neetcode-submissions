a4459be4891ff89609fea44ce18aa05449d76824
Felipe Jiménez <55604046+felipejimenez-ai@users.noreply.github.com>
2026-05-12 22:00:32 -0500

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -1,33 +1,18 @@
-from ast import parse
-import re
-from turtle import end_fill
-from typing import List
-from unittest import result
-
 class Solution:
-    """
-    Join the words with a key delimiter such as #
-    - With the delimiter put the string length, so you
-      know when the word starts and ends.
-
-    Separate the string having a key delimiter such as #
-    - Do a split with the key delimiter and then iterate the array
-      and get the string length with the key delimiter.
-    """
 
     def encode(self, strs: List[str]) -> str:
-        parts = []
-        for word in strs:
-            parts.append(f"-{len(word)}-{word}")
-        return "".join(parts)
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
 
     def decode(self, s: str) -> List[str]:
-        parts = s.split("-")
-        i = 1 # Skip first empty string
-        result = []
-        while i < len(parts):
-            length = parts[i]
-            word = parts[i + 1]
-            result.append(word)
-            i += 2
-        return result
+        res, i = [], 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i : j])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return res
